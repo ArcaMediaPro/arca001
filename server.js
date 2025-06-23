00d6de936be3bb9bcdd01d2de6163176126e6c33
unknown <nardullijavier@gmail.com>
2025-06-23 15:51:44 -0300

    server 002

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// server.js (BASE FUNCIONAL)
+// server.js (Prueba1)
+require('dotenv').config(); // Añadido
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser'); // Añadido
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log("--- INICIANDO SERVIDOR ---");
+console.log("--- PROBANDO: dotenv y cookie-parser ---");
+
+app.use(cookieParser()); // Añadido
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'promocional.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, '0.0.0.0', () => console.log(`>>> Servidor corriendo en el puerto ${PORT}`));
