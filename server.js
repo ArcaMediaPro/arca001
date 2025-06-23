6f7d325ec84b1346b1f00102d372a658a0d8c61c
unknown <nardullijavier@gmail.com>
2025-06-23 15:09:56 -0300

    Test 00

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'promocio
 
 
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.get('/api', (req, res) => res.send('API del Catalogador funcionando!'));
 
